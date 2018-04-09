90c91a7a90b9b55d31863e9789d93b31c1c4351e
Anvita Jain <ajain12@ncsu.edu>
2018-04-09 11:50:12 -0700

    Comments

--- src/main/java/com/playlistExtractor/app/RelevantPlaylistImpl.java
+++ src/main/java/com/playlistExtractor/app/RelevantPlaylistImpl.java
@@ -13,23 +13,15 @@ import com.pllaylistExtractor.utils.Processor;
  * Implementation for getting relevant playlists. 
  */
 public class RelevantPlaylistImpl implements RelevantPlaylist{ 
-	/*Wrote this main method to run the program locally for debugging purpose. 
-	* Should not be shipped with jar.
-	*/
+	/*  Wrote this main method to run the program locally for debugging purpose. 
+	 *  Should not be shipped with jar. Remove this when shipping out. 
+	 */
 	
-	public static void main( String[] args ) throws IOException, InvalidInputException
-    {
-	   
-	 Processor processor = new Processor();
-	 InputFileObject inputObjectFromFile = processor.getModelFromFile("modelData.json");
-	 
-	 
-	 System.out.println(inputObjectFromFile.toString());
-	 System.out.println(processor.processPlaylist("MI3", "UK", inputObjectFromFile));
-	 System.out.println(processor.processPlaylist("MI3", "US", inputObjectFromFile));
-	 System.out.println(processor.processPlaylist("MI3", "CA", inputObjectFromFile));
-	 System.out.println(processor.processPlaylist("StarWars", "IND", inputObjectFromFile));
+  public static void main( String[] args ) throws IOException, InvalidInputException{
+	 RelevantPlaylistImpl impl = new RelevantPlaylistImpl();
+	 System.out.println(impl.getRelevantPlaylist(args[0], args[1], args[2]));
     }
+   /* Method to be used by clients */
    
    public Set<ArrayList<String>> getRelevantPlaylist(String content, String country, String jsonFileInput) throws JsonParseException, JsonMappingException, NullPointerException, IOException, InvalidInputException {
 	   Processor processor = new Processor();
